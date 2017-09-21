e8c472fc9ac5e1ca6b6b0421078d7b9bf17bd717
Gordon Moseley <moseleygj@gmail.com>
2017-09-21 10:42:53 -0400

    Add files via upload

---
+++ math.php
@@ -0,0 +1,30 @@
+<?php 
+// Capitalize a string
+function multiply( $x, $y )
+{
+	$result=$x*$y;
+	echo $result;
+}
+function add( $x, $y )
+{
+	$result=$x+$y;
+	echo $result;
+}
+function subtract( $x, $y )
+{
+	$result=$x-$y;
+	echo $result;
+}
+
+function divide( $x, $y )
+{
+	$result=$x/$y;
+	echo $result;
+}
+function salary( $x )
+{
+	$result=$x/52/5/8
+	echo $result;
+}
+
+?>
