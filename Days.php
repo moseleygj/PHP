3c37df5f4cabf1b1c2df5aa657a08aecc8e0d339
Gordon Moseley <moseleygj@gmail.com>
2017-09-22 23:56:34 -0400

    Add files via upload

---
+++ Days.php
@@ -0,0 +1,9 @@
+<?php
+
+$daysOfWeek = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+$days=count($daysOfWeek);
+for ($i=0;$i<$days;$i++)
+{
+	echo "$daysOfWeek[$i] <br/>";
+}
+?>
