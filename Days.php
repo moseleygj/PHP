2f332117d68bde140de1803bca2ba02ea8777e0a
Gordon Moseley <moseleygj@gmail.com>
2017-09-23 00:01:30 -0400

    Update Days.php

--- Days.php
+++ Days.php
@@ -4,6 +4,12 @@ $daysOfWeek = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday",
 $days=count($daysOfWeek);
 for ($i=0;$i<$days;$i++)
 {
+	if ($i == 0|| $i==6){
+		echo "<font color=\"red\">It's the weekend, Relax!</font><br/>";
+		continue;
+		}
+		
 	echo "$daysOfWeek[$i] <br/>";
+
 }
 ?>
