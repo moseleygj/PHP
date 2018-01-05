7c3f60138a4489e87f80aff65e2af59f66d58560
moseleygj <moseleygj@gmail.com>
2018-01-05 17:29:42 -0500

    NumberStatus Logic fixed

--- NumberStats/main.php
+++ NumberStats/main.php
@@ -1,24 +1,16 @@
-
-
 <html>
 <head>
 <title></title>
 <meta charset="UTF-8">
 </head>
 <style type="text/css">
-
 </style>
-
 <body>
-
 <p id="info"><p>
-<!--<button onclick="work()">Click Me</button> -->
-<div class=""></div>
 
- <?php
+<?php
 $myfile = fopen("fakeNumbers.txt", "r") or die("Unable to open file!");
 $testVar= fread($myfile,filesize("fakeNumbers.txt") . "<br/>");
-
 $h= str_split($testVar);
 
 $num= array(
@@ -33,75 +25,58 @@ $num= array(
 "nine"=>0,
 "zero"=>0);
 
-foreach ($h as $number){
-if ($number ==" " || $number =="" || $number =="\n")
-{continue;}
+$arrlength = count($h);
 
-
-switch($number)
+foreach ($h as $number)
 {
-case 1 : $num[1]++;echo "[ONE]<br/>" ;
-	break;
-case 2 : $num[2]++;echo "[TWO]<br/>" ;
-	break;
-case 3 : $num[3]++;echo "[THREE]<br/>" ;
-	break;
-case 4 :$numb[4]++;echo "[FOUR]<br/>" ;
-	break;
-case 5 : $num[5]++;echo "[FIVE]<br/>" ;
-	break;
-case 6 : $num[6]++;echo "[SIX]<br/>" ;
-	break;
-case 7 : $num[7]++;echo "[SEVEN]<br/>" ;
-	break;
-case 8 : $num[8]++;echo "[EIGHT]<br/>" ;
-	break;
-case 9 : $num[9]++;echo "[NINE]<br/>" ;
-	break;
-case 0 : $num[0]++;echo "[ZERO]<br/>" ;
-	break;
-	//case " ": continue;
+	if ($number ==" " || $number =="" || $number =="\n")
+	{
+	continue;
+	}
+//tally the repetitons of each number
+	switch($number)
+	{
+	case 0 : $num[0]++;echo "[ZERO]<br/>" ;
+		break;
+	case 1 : $num[1]++;echo "[ONE]<br/>" ;
+		break;
+	case 2 : $num[2]++;echo "[TWO]<br/>" ;
+		break;
+	case 3 : $num[3]++;echo "[THREE]<br/>" ;
+		break;
+	case 4 :$numb[4]++;echo "[FOUR]<br/>" ;
+		break;
+	case 5 : $num[5]++;echo "[FIVE]<br/>" ;
+		break;
+	case 6 : $num[6]++;echo "[SIX]<br/>" ;
+		break;
+	case 7 : $num[7]++;echo "[SEVEN]<br/>" ;
+		break;
+	case 8 : $num[8]++;echo "[EIGHT]<br/>" ;
+		break;
+	case 9 : $num[9]++;echo "[NINE]<br/>" ;
+		break;
 	default:continue;
-
+	}
 }
 
-
-}
 echo "<hr/>";
-echo "num[1]: " . $num[1] . ", ";
-//$zero=0;$one=0;$two=0;$three=0;
-//$four=0;$five=0;$six=0;
-//$seven=0;$eight=0;$nine=0;
+//echo "num[1]: " . $num[1] . ", ";
 
-
-
-rsort($num);
-/*
-echo "Total ONE . . . . :. $num[1]<br/>";
-echo "Total 2: $num[2]<br/>";
-echo "Total 3: $num[3]<br/>";
-echo "Total 4: $num[4]<br/>";
-echo "Total 5: $num[5]<br/>";
-echo "Total 6: $num[6]<br/>";
-echo "Total 7: $num[7]<br/>";
-echo "Total 8: $num[8]<br/>";
-echo "Total 9: $num[9]<br/>";
-echo "Total 0: $num[0]<br/>";
+//value of the tallied  counters:
+/*for ($i=0;$i<$arrlength;$i++)
+{
+echo $num[$i] . "<br/>";
+}
 */
-fclose($myfile);
+arsort($num);
+
+foreach($num as $x => $value)
+{
+   echo "Key=" . $x . ", Value=" . $value . "<br/>";
+}
 ?> 
 </body>
-
 <script type="text/javascript">
-var numberHolder=10;
-function work()
-{
-for (i=0; i <numberHolder;i++)
-{
-document.getElementById("info").innerHTML +="butt"+" <br/>";
-}
-}
 </script>
-
 </html>
-
